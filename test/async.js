4a2b60412461e7cec72c5ac538b0225593b24353
arelive <me@are.moe>
2023-06-08 19:11:35 +0300

    update tests

--- test/async.js
+++ test/async.js
@@ -30,37 +30,46 @@ const queue = async.queue(
 )
 
 for (let i = 0; i < nQuotes; i++) {
-  const text = lorem.generateParagraphs(1)
-  const username = lorem.generateWords(2)
-  const avatar = 'https://telegra.ph/file/59952c903fdfb10b752b3.jpg'
-
   const json = {
     botToken: process.env.BOT_TOKEN,
-    type: 'quote',
-    format: 'png',
-    backgroundColor: '#FFFFFF',
+    backgroundColor: '',
     width: 512,
     height: 768,
     scale: 2,
-    messages: [
-      {
-        entities: [],
-        avatar: true,
-        from: {
-          id: 1,
-          name: username,
-          photo: {
-            url: avatar
-          }
-        },
-        text: text,
-        replyMessage: {}
-      }
-    ]
+    messages: Array.from({ length: i % 5 + 1 }, () => ({
+      entities: [],
+      avatar: true,
+      from: {
+        id: Math.floor(Math.random() * 100),
+        name: lorem.generateWords(2),
+        photo: {
+          url: 'https://telegra.ph/file/59952c903fdfb10b752b3.jpg'
+        }
+      },
+      text: lorem.generateParagraphs(1),
+      replyMessage: {}
+    }))
   }
 
   console.time(i)
-  queue.push(json, res => {
+
+  queue.push({
+    ...json,
+    type: 'quote',
+    format: 'webp'
+  }, res => {
+    const buffer = Buffer.from(res.image, 'base64')
+    fs.writeFile(
+      path.resolve(`./test/${i}.webp`), buffer,
+      err => err && console.error(err)
+    )
+  })
+
+  queue.push({
+    ...json,
+    type: 'image',
+    format: 'png'
+  }, res => {
     const buffer = Buffer.from(res.image, 'base64')
     fs.writeFile(
       path.resolve(`./test/${i}.png`), buffer,
