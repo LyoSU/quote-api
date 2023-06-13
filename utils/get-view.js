ac87509790c6d63afbba2989867a0cec19d14d96
arelive <me@are.moe>
2023-06-13 18:28:45 +0300

    refactor: cache views, separate css and html

--- utils/get-view.js
+++ utils/get-view.js
@@ -2,10 +2,24 @@ const path = require('path')
 const fs = require('fs')
 const Handlebars = require('handlebars')
 
-module.exports = viewName => {
+const cache = {}
+
+module.exports = (viewName, styleName) => {
+  const cacheKey = `${viewName}/${styleName}`
+  if (cache[cacheKey]) {
+    return cache[cacheKey]
+  }
+
   const template = fs.readFileSync(
     path.resolve(`./views/${viewName}.hbs`)
   ).toString('utf-8')
 
-  return Handlebars.compile(template)
+  const style = fs.readFileSync(
+    path.resolve(`./views/${styleName}.css`)
+  ).toString('utf-8')
+
+  const view = Handlebars.compile(template.replace('{{> style}}', style))
+
+  cache[cacheKey] = view
+  return view
 }
